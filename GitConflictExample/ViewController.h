f5600e69f81840a02e44b2b45630e19c56a1ee5c
Daniel Mathews <dannyl.mathews@gmail.com>
2016-04-01 16:32:39 -0700

    starter project

---
+++ GitConflictExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitConflictExample
+//
+//  Created by Daniel Mathews on 2016-04-01.
+//  Copyright © 2016 Daniel Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
